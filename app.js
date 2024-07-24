38db0afe3d4bc1b093da44b5895d20d7d388165b
akash20042 <akashprinsh728@gmail.com>
2024-07-24 07:40:58 +0530

    episode1

---
+++ app.js
@@ -0,0 +1,7 @@
+ 
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+ 
+ const  head = React.createElement("h1" ,{}, " hi i am aksh kumar 🚀🚀🚀😒😒😒😒");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+     root.render(head);
